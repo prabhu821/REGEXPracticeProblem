a332ba5bd165f907bbfe1c6d50a0c1c4bff9bfe6
rishiraj007 <rishiraj@mounttalent.com>
2023-06-23 16:39:47 +0530

    [Prabhu]Add: check string that has 'a' followed by 2 or 3 'b'

--- REGEXPracticeProblem/Program.cs
+++ REGEXPracticeProblem/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("REGEX Practice Problem!");
+            Validate validate = new Validate();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter option to choose \n1.Check String \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nEnter string that has an ‘a’ followed by two to three 'b'");
+                        string str = Console.ReadLine();
+                        validate.validateString(str);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
